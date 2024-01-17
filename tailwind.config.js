e4dd581954d7cfbce33a841ddc25e75e2f12485b
karl-jerico <jerico.jeks@gmail.com>
2024-01-17 13:14:22 +0800

    setup environment

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./sections/**/*.{ts,tsx}",
+    "./styles/**/*.{ts,tsx}",
+  ],
+}
