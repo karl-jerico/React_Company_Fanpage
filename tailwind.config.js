b018b8c89115343bf86bb1a40e2917a55e705faa
karl-jerico <jerico.jeks@gmail.com>
2024-01-18 11:26:25 +0800

    restructure color variable

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ export default {
       backgroundImage: {
         "welcome-bg": "url('/src/assets/hero-bg.png')",
       },
+      colors: {
+        "orange": "#DF6951",
+        "black": "#212832",
+        "cream": "#FFF1DA",
+        "darkblue": "#181E4B",
+        "light-orange": "#F1A501",
+        "gray": "#5E6282",
+
+      }
     }
   }
 }
