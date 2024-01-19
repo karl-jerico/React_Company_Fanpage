52e5dacb385aae208807e7ed47de4f82121403d7
karl-jerico <jerico.jeks@gmail.com>
2024-01-19 14:41:58 +0800

    Add assets

---
+++ src/assets/all_teams.tsx
@@ -0,0 +1,43 @@
+import ase from "./ase.png";
+import boiz from "./boiz.png";
+import girls from "./girls.png";
+import loca from "./loca.png";
+import teco from "./teco.png";
+import umed from "./umed.png";
+
+const all_teams = () => {
+  return [
+    {
+      id: 1,
+      name: "ase",
+      image: ase,
+    },
+    {
+      id: 2,
+      name: "boiz",
+      image: boiz,
+    },
+    {
+      id: 3,
+      name: "girls",
+      image: girls,
+    },
+    {
+      id: 4,
+      name: "loca",
+      image: loca,
+    },
+    {
+      id: 5,
+      name: "teco",
+      image: teco,
+    },
+    {
+      id: 6,
+      name: "umed",
+      image: umed,
+    },
+  ];
+};
+
+export default all_teams;
